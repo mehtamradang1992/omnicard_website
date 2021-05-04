e33fa5f9e86a5bb5b8bf8a88be9da801418fa1fe
Deepa <deepa.tyagi@eroute.in>
2021-05-04 15:17:25 +0530

    faq added

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,22 @@ function step(startTime) {
             animateOut: "fadeOut",
             animateIn: "fadeIn"
         });
+           //Faq
+      $('.faq--area .faq-title').on('click', function (e) {
+        var element = $(this).parent('.faq--item');
+        if (element.hasClass('open')) {
+          element.removeClass('open');
+          element.find('.faq-content').removeClass('open');
+          element.find('.faq-content').slideUp(300, "swing");
+        } else {
+          element.addClass('open');
+          element.children('.faq-content').slideDown(300, "swing");
+          element.siblings('.faq--item').children('.faq-content').slideUp(300, "swing");
+          element.siblings('.faq--item').removeClass('open');
+          element.siblings('.faq--item').find('.faq-title').removeClass('open');
+          element.siblings('.faq--item').find('.faq-content').slideUp(300, "swing");
+        }
+      });
         $('.banner-1-slider').on('translate.owl.carousel', function(e){
             idx = e.item.index;
             if(idx==3 || idx==6 ){
